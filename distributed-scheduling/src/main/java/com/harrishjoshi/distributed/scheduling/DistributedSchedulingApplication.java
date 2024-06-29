93cfdf51d5bbc06e04ede74f524f24932cd7661a
harrishjoshi <dev.harishjoshi@gmail.com>
2024-06-29 08:42:08 +0545

    chore: Distributed scheduling project setup

---
+++ distributed-scheduling/src/main/java/com/harrishjoshi/distributed/scheduling/DistributedSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.harrishjoshi.distributed.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSchedulingApplication.class, args);
+	}
+
+}
---
+++ distributed-scheduling/src/test/java/com/harrishjoshi/distributed/scheduling/DistributedSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrishjoshi.distributed.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
